c77d53f049948759825e76f216a495e1184b0194
Maxime Leroy <maxime.leroy@6wind.com>
2015-05-06 20:26:53 +0200

    tests: add unit tests for ovs-fp ml2 mech
    Units tests based on the official mech_openvswitch tests.
    
    "tox -e py27" should return the following output:
    OVSFPMechanismVlanTestCase.test_type_vlan_bad [0.077848s] ... ok
    OVSFPMechanismSGDisabledLocalTestCase.test_type_local [0.074092s] ... ok
    OVSFPMechanismGreTestCase.test_type_gre [0.077582s] ... ok
    OVSFPMechanismGenericTestCase.test_unknown_type [0.074716s] ... ok
    OVSFPMechanismGreTestCase.test_type_gre_bad [0.076943s] ... ok
    OVSFPMechanismVlanTestCase.test_type_vlan [0.077639s] ... ok
    OVSFPMechanismSGDisabledLocalTestCase.test_type_local_dead [0.075834s] ... ok
    OVSFPMechanismLocalTestCase.test_type_local [0.116571s] ... ok
    OVSFPMechanismFlatTestCase.test_type_flat_bad [0.116716s] ... ok
    OVSFPMechanismFlatTestCase.test_type_flat [0.120457s] ... ok
    OVSFPMechanismLocalTestCase.test_type_local_dead [0.120383s] ... ok
    
    Change-Id: Iea598e1417ded9a84281816ce55c7924bf290544

---
+++ networking_6wind/tests/unit/ml2_drivers/test_mech_ovs_fp.py
@@ -0,0 +1,85 @@
+# Copyright 2015 6WIND S.A.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo.config import cfg
+
+from networking_6wind.common import constants
+from networking_6wind.common.utils import get_vif_vhostuser_socket
+from networking_6wind.ml2_drivers import mech_ovs_fp
+
+from neutron.extensions import portbindings
+from neutron.tests.unit.plugins.ml2 import _test_mech_agent as base
+from neutron.tests.unit.plugins.ml2.drivers import (
+    test_mech_openvswitch as test_ovs)
+
+
+class OVSFPMechanismBaseTestCase(test_ovs.OpenvswitchMechanismBaseTestCase):
+    VIF_TYPE = constants.VIF_TYPE_VHOSTUSER
+    VIF_DETAILS = {portbindings.CAP_PORT_FILTER: True,
+                   portbindings.OVS_HYBRID_PLUG: True,
+                   constants.VIF_PLUGIN_SCRIPT: "vif-ovs-fp-plug",
+                   constants.VIF_VHOSTUSER_SOCKET: get_vif_vhostuser_socket(
+                       base.PORT_ID)}
+
+    def setUp(self):
+        super(OVSFPMechanismBaseTestCase, self).setUp()
+        self.driver = mech_ovs_fp.OVSFPMechanismDriver()
+        self.driver.initialize()
+
+
+class OVSFPMechanismSGDisabledBaseTestCase(
+    OVSFPMechanismBaseTestCase):
+    VIF_DETAILS = {portbindings.CAP_PORT_FILTER: False,
+                   portbindings.OVS_HYBRID_PLUG: False,
+                   constants.VIF_PLUGIN_SCRIPT: "vif-ovs-fp-plug",
+                   constants.VIF_VHOSTUSER_SOCKET: get_vif_vhostuser_socket(
+                       base.PORT_ID)}
+
+    def setUp(self):
+        cfg.CONF.set_override('enable_security_group',
+                              False,
+                              group='SECURITYGROUP')
+        super(OVSFPMechanismSGDisabledBaseTestCase, self).setUp()
+
+
+class OVSFPMechanismGenericTestCase(OVSFPMechanismBaseTestCase,
+                                    base.AgentMechanismGenericTestCase):
+    pass
+
+
+class OVSFPMechanismLocalTestCase(OVSFPMechanismBaseTestCase,
+                                  base.AgentMechanismLocalTestCase):
+    pass
+
+
+class OVSFPMechanismFlatTestCase(OVSFPMechanismBaseTestCase,
+                                 base.AgentMechanismFlatTestCase):
+    pass
+
+
+class OVSFPMechanismVlanTestCase(OVSFPMechanismBaseTestCase,
+                                 base.AgentMechanismVlanTestCase):
+    pass
+
+
+class OVSFPMechanismGreTestCase(OVSFPMechanismBaseTestCase,
+                                base.AgentMechanismGreTestCase):
+    pass
+
+
+class OVSFPMechanismSGDisabledLocalTestCase(
+    OVSFPMechanismSGDisabledBaseTestCase,
+    base.AgentMechanismLocalTestCase):
+    pass
