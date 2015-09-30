9090adf690d8c042c58165adedaf43165c56a83a
Maxime Leroy <maxime.leroy@6wind.com>
2015-09-30 15:40:02 +0200

    use hotplug feature to create/delete new vhostuser port
    Change-Id: I38f051c0d0c7bc326eb483a9cd68cb4fdb2e1528

--- networking_6wind/plug_scripts/vhostuser.py
+++ networking_6wind/plug_scripts/vhostuser.py
@@ -24,142 +24,28 @@ from nova.utils import execute
 
 from networking_6wind.plug_scripts import utils
 
-
-# -----------------------------------------------------------------------------
-DEFAULT_VHOSTUSER_IFACENAME = 'tap_unused_%d'
-
-
-class VhostUserPort(object):
-    """A class to represent a vhostuser port"""
-
-    def __init__(self, port_id, iface_name):
-        """Create a VhostUserPort
-
-        :arg int portid:
-             DPDK portid of the vhostuser device on the hypervisor
-        :arg str iface_name:
-            Name of the vhostuser device on the hypervisor.
-        """
-
-        self.port_id = port_id
-        self.iface_name = iface_name
-
-# -----------------------------------------------------------------------------
-FP_SHMEM_PORTS_RE = re.compile('port (?P<port_id>\d+):.*?(pmd-vhost).*'
-                               ' (?P<iface>\S+)$')
-
-
-def __get_available_vhostuser_port():
-    """Use fp-shmem-port to get an available vhostuser port on the host
-
-    :returns:
-        a VhostUserPort object
-    """
-    try:
-        out, err = execute('fp-shmem-ports', '-d', '-e', 'disabled',
-                           '--driver', 'pmd-vhost', run_as_root=True)
-    except processutils.ProcessExecutionError as exception:
-        utils.log_exception('Failed to run fp-shem-ports -d', exception)
-        return None
-
-    for match in FP_SHMEM_PORTS_RE.finditer(out):
-        port_id = int(match.group('port_id'))
-        iface_name = match.group('iface')
-
-        return VhostUserPort(port_id=port_id, iface_name=iface_name)
-
-    return None
-
-
-def __get_used_vhostuser_port(iface_name):
-    """Use fp-shmem-port to find a used vhostuser port from this ifname
+def create_vhostuser_port(iface_name, socket_path):
+    """Create a new vhostuser port:
 
     :arg str iface_name:
-         Name of the interface of the vhostuser port to find
-    :returns:
-        a VhostUserPort object
-    """
-    try:
-        out, err = execute('fp-shmem-ports', '-d', '-e', 'enabled',
-                           '--driver', 'pmd-vhost', run_as_root=True)
-    except processutils.ProcessExecutionError as exception:
-        utils.log_exception('Failed to run fp-shem-ports -d', exception)
-        return None
-
-    for match in FP_SHMEM_PORTS_RE.finditer(out):
-        if match.group('iface') == iface_name:
-            port_id = int(match.group('port_id'))
-            iface_name = match.group('iface')
-
-            return VhostUserPort(port_id=port_id, iface_name=iface_name)
-
-    return None
-
-
-# -----------------------------------------------------------------------------
-def count_vhostuser_ports(select='all'):
-    """Use fp-shmem-port to count the number of vhostuser ports on the host
-
-    :arg str select:
-         'all' to select all the vhostuser ports
-         'enabled' to select all the enabled vhostuser ports
-                   (i.e. port already used by a VM)
-         'disabled' to select all the disabled vhostuser ports
-                   (i.e. port available)
-    """
-    out = None
-    try:
-        out, err = utils.execute('fp-shmem-ports', '-c', '-e', select,
-                                 '--driver', 'vhost-pmd', run_as_root=True)
-    except processutils.ProcessExecutionError as exception:
-        utils.log_exception('Failed to run fp-shem-ports -c', exception)
-        return 0
-
-    return int(out)
-
-
-def get_vhostuser_port(new_iface, socket_path):
-    """Get an available vhostuser port:
-
-    :arg str new_iface:
-    the iface name to set for the reserved vhostuser port
-    :arg int nb_queues:
-    number of queues to configure on the vhostuser port
-
+    the iface name of the vhostuser port to create
+    :arg str socket_path:
+    the vhostuser socket path
     :returns:
     a vhostuser object if success or None if there is no available port
     """
-    vhostuser_port = __get_used_vhostuser_port(new_iface)
-    if vhostuser_port:
-        return vhostuser_port
-
-    vhostuser_port = __get_available_vhostuser_port()
-    if vhostuser_port is None:
-        return None
-
     devargs = "sockmode=client,sockname=%s" % socket_path
 
-    utils.set_down_iface(vhostuser_port.iface_name)
-    utils.rename_iface(vhostuser_port.iface_name, new_iface)
-    utils.enable_dpdk_port(vhostuser_port.port_id, 'all', devargs)
-    utils.set_up_iface(new_iface)
+    if not utils.device_exists(iface_name):
+        utils.new_dpdk_virtual_port('pmd-vhost', devargs, iface_name)
+        utils.set_up_iface(iface_name)
 
-    return vhostuser_port
+def delete_vhostuser_port(iface_name):
+    """Delete a vhostuser port
 
-
-def put_vhostuser_port(iface_name):
-    """Put back a vhostuser port in the available pool used by a vm
-
-    :arg str iface:
-    the iface name of the vhostuser port to put back in the available pool
-
+    :arg str iface_name:
+    the iface name of the vhostuser port to delete
     """
-    vhostuser_port = __get_used_vhostuser_port(iface_name)
-    if vhostuser_port is None:
-        return None
-
-    # rename with default name
-    utils.set_down_iface(vhostuser_port.iface_name)
-    utils.disable_dpdk_port(vhostuser_port.port_id)
-    new_iface_name = DEFAULT_VHOSTUSER_IFACENAME % vhostuser_port.port_id
-    utils.rename_iface(vhostuser_port.iface_name, new_iface_name)
+    if utils.device_exists(iface_name):
+        utils.set_down_iface(iface_name)
+        utils.free_dpdk_port(iface_name)
