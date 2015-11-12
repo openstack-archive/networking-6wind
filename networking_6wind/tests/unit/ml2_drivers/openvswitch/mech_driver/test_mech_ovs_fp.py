89bdd9f087fcebe2d42d100f0ce6360540d9d9ae
Maxime Leroy <maxime.leroy@6wind.com>
2015-11-12 18:03:10 +0100

    move mech_ovs_fp.py into ml2_drivers/openvswitch/mech_driver
    Let's have a similar tree that the official neutron has for
    mechanism driver.
    
    Change-Id: I4cffc9d663e424a52a9b303a90c84c3991d6d7fd

--- networking_6wind/tests/unit/ml2_drivers/openvswitch/mech_driver/test_mech_ovs_fp.py
+++ networking_6wind/tests/unit/ml2_drivers/openvswitch/mech_driver/test_mech_ovs_fp.py
@@ -17,7 +17,7 @@ from oslo_config import cfg
 
 from networking_6wind.common import constants
 from networking_6wind.common.utils import get_vif_vhostuser_socket
-from networking_6wind.ml2_drivers import mech_ovs_fp
+from networking_6wind.ml2_drivers.openvswitch.mech_driver import mech_ovs_fp
 
 from neutron.extensions import portbindings
 from neutron.tests.unit.plugins.ml2 import _test_mech_agent as base
