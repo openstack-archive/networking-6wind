7e9ef11baaf7ee54f7a4cb39b9774d3ffb1b22f9
Francesco Santoro <francesco.santoro@6wind.com>
2016-05-17 11:45:40 +0200

    devstack: update fastpath installation
    This commits updates devstack script for fastpath installation
    and configuaration to:
    - support installation via credentials package
    - switch to xenial for tests that means nomore need for ppa
    - reworking names and methods
    
    Signed-off-by: Francesco Santoro <francesco.santoro@6wind.com>
    
    Change-Id: I80e64365295086a0bc41e716f1b49704c976a5b5

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -38,17 +38,10 @@ function create_nova_rootwrap {
 }
 
 function configure_ml2_for_fast_path {
-    if [[ "$Q_DISABLE_SECURITY" == "True" ]]; then
-        # disable firewall in nova
-        LIBVIRT_FIREWALL_DRIVER=nova.virt.firewall.NoopFirewallDriver
-        iniset $NOVA_CONF DEFAULT firewall_driver $LIBVIRT_FIREWALL_DRIVER
-        inicomment $NOVA_CONF DEFAULT security_group_api
-        # disable firewall in neutron
-        iniset /$Q_PLUGIN_CONF_FILE securitygroup enable_security_group False
-        iniset /$Q_PLUGIN_CONF_FILE securitygroup firewall_driver neutron.agent.firewall.NoopFirewallDriver
-    elif [[ "$Q_USE_SECGROUP" == "True" ]]; then
-        iniset /$Q_PLUGIN_CONF_FILE securitygroup enable_ipset False
-        iniset $NEUTRON_CONF agent comment_iptables_rules False
+    if [[ "$Q_USE_SECGROUP" == "False" ]]; then
+        iniset /$Q_PLUGIN_CONF_FILE securitygroup firewall_driver noop
+    else
+        iniset /$Q_PLUGIN_CONF_FILE securitygroup firewall_driver iptables_hybrid
     fi
 }
 
@@ -64,20 +57,28 @@ function nova_enable_monkey_patch {
         nova.virt.libvirt.vif:networking_6wind.monkey_patch.vif.decorator
 }
 
+function nova_enable_vcpu_pinning {
+    pin_set=$(python /usr/local/bin/get_vcpu_pin_set.py)
+    iniset $NOVA_CONF DEFAULT vcpu_pin_set "$pin_set"
+}
+
 # main loop
 if is_service_enabled net-6wind; then
     source $NET_6WIND_DIR/devstack/libs/fast-path
 
-    if [[ "$1" == "stack" && "$2" == "install" ]]; then
+    if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
         if [[ "$OFFLINE" != "True" ]]; then
-            install_fast_path
+            setup_va_repo
+            install_va
         fi
-        write_fast_path_conf
-        start_fast_path
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        write_va_conf
+        start_va
         setup_develop $DEST/networking-6wind
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         if is_service_enabled nova; then
             create_nova_rootwrap
+            nova_enable_vcpu_pinning
         fi
         if is_service_enabled neutron; then
             configure_ml2_for_fast_path
@@ -90,12 +91,13 @@ if is_service_enabled net-6wind; then
     fi
 
     if [[ "$1" == "unstack" ]]; then
-        stop_fast_path
         stop_rpc_fp_server
+        stop_va
     fi
 
     if [[ "$1" == "clean" ]]; then
-        uninstall_fast_path
+        uninstall_va
+        remove_va_repo
     fi
 fi
 
