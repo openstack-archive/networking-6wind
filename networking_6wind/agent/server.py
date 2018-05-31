55fc945d79894374f7c799ef4f4c466502042a79
Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
2018-05-31 18:25:40 +0200

    agent/server: use _LE translator from networking_6wind
    * Use translator object for log ERROR messages from local _i18n
      submodule.
    * Reorganise imports.
    
    Change-Id: Id78647c8cb0edc4b98d066557f4cacc254259517
    Signed-off-by: Valentina <valentina.krasnobaeva@6wind.com>
    Acked-by: Maxime Leroy <maxime.leroy@6wind.com>

--- networking_6wind/agent/server.py
+++ networking_6wind/agent/server.py
@@ -15,11 +15,10 @@
 import datetime
 import os
 
+from networking_6wind._i18n import _LE
 from networking_6wind.common import constants
 
-from neutron._i18n import _LE
 from neutron.agent import rpc as agent_rpc
-
 from neutron import manager
 
 from neutron_lib.agent import topics
