3344a2f8ae55cf896b3e8c81dafd1ef2a4fde58c
Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
2018-12-19 17:43:17 +0100

    ml2_drivers/mech_driver: remove internal cache
    ml2_drivers/mech_driver:
    
        * no need to cache fp_info during fp_info_max_age interval, just
          call _update_fp_info before trying to bind segment for agent, to
          check if fp-rte is alive;
    
        * update doc strings for OVSFPMechanismDriver and
          LBFPMechanismDriver classes.
    
    common/config:
    
        * delete support of fp_info_max_age parameter from ml2_conf.ini and
          add support of tunnel_types, because we needed them, when allowed
          network types are checked for agent.
    
    Change-Id: Iace8a14205338f2ac90ffba1512f1b06555eeabb
    Signed-off-by: Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
    Acked-by: Maxime Leroy <maxime.leroy@6wind.com>

--- networking_6wind/ml2_drivers/linuxbridge/mech_driver/mech_lb_fp.py
+++ networking_6wind/ml2_drivers/linuxbridge/mech_driver/mech_lb_fp.py
@@ -11,10 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-from oslo_config import cfg
 from oslo_log import log
-
-import datetime
 
 from networking_6wind.common import constants
 from networking_6wind.common.utils import get_vif_vhostuser_socket
@@ -26,112 +23,64 @@ from neutron_lib.plugins.ml2 import api
 
 
 LOG = log.getLogger(__name__)
-cfg.CONF.import_group('ml2_fp', 'networking_6wind.common.config')
 
 
 class LBFPMechanismDriver(mech_linuxbridge.LinuxbridgeMechanismDriver):
-    """Attach to networks using linuxbridge L2 agent.
+    """Attach to networks using neutron-fastpath-agent L2 agent.
 
-    The OVSFPMechanismDriver integrates the ml2 plugin with the fast path
-    linuxbridge L2 agent. Port binding with this driver requires the lb fp
-    agent to be running on the port's host, and that agent to have connectivity
+    The LBFPMechanismDriver integrates Ml2Plugin class with
+    neutron-fastpath-agent and neutron-linuxbridge-agent.
+    Port binding with this driver requires neutron-fastpath-agent and
+    neutron-linuxbridge-agent to be run
+    on the port's host, and these agent should have connectivity
     to at least one segment of the port's network.
     """
 
     def __init__(self):
         super(LBFPMechanismDriver, self).__init__()
-
-        self.conf = cfg.CONF.ml2_fp
-        self.fp_info_max_age = self.conf.fp_info_max_age
-        self.fp_info = {
-            'timestamp': constants.BASE_TIMESTAMP,
-            'product': 'unknown',
-            'product_version': 'unknown',
-            'active': False,
-            'vhostuser_socket_dir': '',
-            'vhostuser_socket_prefix': '',
-            'vhostuser_socket_mode': '',
-            'supported_plugs': [],
-        }
-        self.is_first_update = True
-        self.needs_update = True
-
-    def _update_fp_info(self, context):
-        LOG.debug('Trying to retrieve fp_info from agent...')
-        for fp_agent in context.host_agents(constants.FP_AGENT_TYPE):
-            LOG.debug("Checking agent: %s", fp_agent)
-            self.fp_info = fp_agent['configurations']
-            LOG.debug('Correctly retrieved fp_info from agent: %s' %
-                      self.fp_info)
-            return
-
+        self.agent_type = constants.FP_AGENT_TYPE
         self.fp_info = None
 
-    def _get_fp_info(self, context):
-        if self.fp_info is None:
-            return
-
-        ts_format = "%Y-%m-%d %H:%M:%S"
-        current_time = datetime.datetime.now().strftime(ts_format)
-        last_update = self.fp_info['timestamp']
-
-        t1 = datetime.datetime.strptime(current_time, ts_format)
-        t2 = datetime.datetime.strptime(last_update, ts_format)
-
-        tdelta = t1 - t2
-        age = tdelta.total_seconds()
-
-        if age >= self.fp_info_max_age:
-            self._update_fp_info(context)
-
     def try_to_bind_segment_for_agent(self, context, segment, agent):
-        if self.needs_update:
-            if self.is_first_update:
-                self._update_fp_info(context)
-                self.is_first_update = False
-            else:
-                self._get_fp_info(context)
-
-        if self.check_segment_for_agent(segment, agent):
-            context.set_binding(segment[api.ID],
-                                self.get_vif_type(context, agent, segment),
-                                self.get_vif_details(context, agent, segment))
-            return True
-        else:
+        if not self.check_segment_for_agent(segment, agent):
             return False
+        LOG.debug("Trying to retrieve fp_info from agent...")
+        self.fp_info = agent.get('configurations')
+        if self.fp_info is None or not self.fp_info['active']:
+            LOG.warning("Can't retrieve fp_info")
+            return False
+        LOG.debug("Correctly retrieved fp_info from agent: %s" % self.fp_info)
+        if portbindings.VIF_TYPE_BRIDGE not in self.fp_info['supported_plugs']:
+            LOG.error("vif_type %s is not supported in ovs-lb ML2 mechanism "
+                      " driver" % portbindings.VIF_TYPE_BRIDGE)
+        context.set_binding(segment[api.ID],
+                            self.get_vif_type(context, agent, segment),
+                            self.get_vif_details(context, agent, segment))
+        return True
 
     def get_vif_type(self, context, agent, segment):
-        if self.fp_info is not None and self.fp_info['active']:
-            return portbindings.VIF_TYPE_VHOST_USER
-
-        return self.vif_type
+        return portbindings.VIF_TYPE_VHOST_USER
 
     def get_vif_details(self, context, agent, segment):
-        VIF_BRIDGE = portbindings.VIF_TYPE_BRIDGE
-        if self.fp_info is not None and self.fp_info['active']:
-            if VIF_BRIDGE in self.fp_info['supported_plugs']:
-                socket_prefix = self.fp_info['vhostuser_socket_prefix']
-                socket_dir = self.fp_info['vhostuser_socket_dir']
-                socket = get_vif_vhostuser_socket(socket_prefix,
-                                                  socket_dir,
-                                                  context.current['id'])
-                brg = constants.BRIDGE_PREFIX + context.network.current['id']
+        socket_prefix = self.fp_info['vhostuser_socket_prefix']
+        socket_dir = self.fp_info['vhostuser_socket_dir']
+        socket = get_vif_vhostuser_socket(socket_prefix, socket_dir,
+                                          context.current['id'])
+        brg = constants.BRIDGE_PREFIX + context.network.current['id']
 
-                if self.fp_info['vhostuser_socket_mode'] == 'client':
-                    qemu_mode = portbindings.VHOST_USER_MODE_SERVER
-                else:
-                    qemu_mode = portbindings.VHOST_USER_MODE_CLIENT
+        if self.fp_info['vhostuser_socket_mode'] == 'client':
+            qemu_mode = portbindings.VHOST_USER_MODE_SERVER
+        else:
+            qemu_mode = portbindings.VHOST_USER_MODE_CLIENT
 
-                details_copy = self.vif_details.copy()
-                details_copy[portbindings.VHOST_USER_SOCKET] = socket
-                details_copy[portbindings.VHOST_USER_MODE] = qemu_mode
-                details_copy[constants.VIF_VHOSTUSER_FP_PLUG] = True
+        details_copy = self.vif_details.copy()
+        details_copy[portbindings.VHOST_USER_SOCKET] = socket
+        details_copy[portbindings.VHOST_USER_MODE] = qemu_mode
+        details_copy[constants.VIF_VHOSTUSER_FP_PLUG] = True
 
-                # Workaroud to notify nova of bridge name to be used during
-                # plug. This should not be necessary because Nova can detect
-                # this bridge name, but some code is missing for the
-                # VIF_VHOSTUSER_FP_PLUG case (in nova/network/neutronv2/api.py)
-                details_copy[portbindings.VIF_DETAILS_BRIDGE_NAME] = brg
-                return details_copy
-
-        return self.vif_details
+        # Workaroud to notify nova of bridge name to be used during
+        # plug. This should not be necessary because Nova can detect
+        # this bridge name, but some code is missing for the
+        # VIF_VHOSTUSER_FP_PLUG case (in nova/network/neutronv2/api.py)
+        details_copy[portbindings.VIF_DETAILS_BRIDGE_NAME] = brg
+        return details_copy
